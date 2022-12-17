37d7b8856cb015557738050f6980839dae468bdd
edofarrell <edo.farrell17@gmail.com>
2022-12-17 21:10:37 +0700

    add comment on writetocsv method

--- Main.java
+++ Main.java
@@ -25,23 +25,24 @@ public class Main {
         double crossoverRate = 0.8;     // probabilitas crossover berhasil
         int elitismCount = 10;          // jumlah individu yang akan dipilih secara elitism
 
-        File folder = new File("./selenium/"+"test case"+"/");
-        File[] input = folder.listFiles();
+        File folder = new File("./selenium/"+"test case"+"/");  // ambil folder yang berisi file input
+        File[] input = folder.listFiles();                      // ambil semua file input
 
-        for (int in = 0; in < input.length; in++) {
-            String tc = "";                      //untuk menampung test case dalam bentuk String
+        for (int in = 0; in < input.length; in++) {     // kerjakan setiap test case
+            String tc = "";                      // untuk menampung test case dalam bentuk String
             Scanner sc = new Scanner(input[in]); // objek scanner
 
             // int n = sc.nextInt();
             int n = 5; // besar papan (nxn)
             int chromosomeLength = n * n; // panjang kromosom = besar papan
 
-            int buang = sc.nextInt(); // varibel untuk membuang 1 angka pertma dari input karena input kelebihan 1
-            // angka(dari web scrapping) yang tidak digunakan
+            // varibel untuk membuang 1 angka pertma dari input karena 
+            // input kelebihan 1 angka(dari web scrapping) yang tidak digunakan
+            int buang = sc.nextInt(); 
 
             int maxFitness = 0;             // variabel untuk menghitung nilai fitness maximum yang bisa didapatkan
             int[][] board = new int[n][n];  // array 2d untuk menyimpan papan permainan (setiap cell diisi dengan angka,
-            // jika cell tersebut kosong maka diisi dengan -1)
+                                            // jika cell tersebut kosong maka diisi dengan -1)
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
                     board[i][j] = sc.nextInt();     // mengisi papan permainan
@@ -75,17 +76,18 @@ public class Main {
             System.out.println("Found solution in " + generation + " generations");
             System.out.println("Best solution: " + population.getFittest(0).toString());
 
-            String filePath = "./result.csv";
-            String PopulationSize = populationSize + "";
-            String MutationRate = String.format("%.4f", mutationRate);
-            String CrossoverRate = crossoverRate + "";
-            String NumberOfGenerations = numOfGeneration + "";
-            String NumberOfElitism = elitismCount + "";
-            String BestChromosome = population.getFittest(0).toString();
-            String FitnessChromosome = population.getFittest(0).getFitness() + "";
-            String Maxfitness = maxFitness + "";
-            String TestCase = tc;
-            writeDataLineByLine(
+            //data untuk menyimpan ke file
+            String filePath = "./result.csv";                   // path file output
+            String PopulationSize = populationSize + "";        // besar populasi
+            String MutationRate = String.format("%.4f", mutationRate);  // probabilitas terjadi mutasi
+            String CrossoverRate = crossoverRate + "";          // probabilitas crossover berhasil
+            String NumberOfGenerations = numOfGeneration + "";  // banyak generasi
+            String NumberOfElitism = elitismCount + "";         // jumlah individu yang akan dipilih secara elitism
+            String BestChromosome = population.getFittest(0).toString(); // individu terbaik
+            String FitnessChromosome = population.getFittest(0).getFitness() + ""; // fitness individu terbaik
+            String Maxfitness = maxFitness + "";    //nilai fitness maksimum untuk test case
+            String TestCase = tc;                   //test case
+            writeDataLineByLine(    //tulis ke file output
                     filePath,
                     PopulationSize,
                     MutationRate,
@@ -113,19 +115,18 @@ public class Main {
             String Maxfitness,
             String TestCase
     ) {
-        //buat file untuk menyimpan hasil eksperimen
+        // buat file untuk menyimpan hasil eksperimen
         File file = new File(filePath);
         try {
 
-            FileWriter outputfile = new FileWriter(file, true); //object filewriter untuk menulis ke file output
+            FileWriter outputfile = new FileWriter(file, true); // object filewriter untuk menulis ke file output
 
-            CSVWriter writer = new CSVWriter(outputfile);       //object csvwriter untuk menulis ke file output dalam format csv
+            CSVWriter writer = new CSVWriter(outputfile);       // object csvwriter untuk menulis ke file output dalam format csv
 
-            //penambahan header pada csv
-            System.out.println("write");
-            BufferedReader br = new BufferedReader(new FileReader(file));
-            if (br.readLine() == null) {
-                String[] header = {
+            // penambahan header pada csv
+            BufferedReader br = new BufferedReader(new FileReader(file));   //object bufferedreader
+            if (br.readLine() == null) {        //jika file belum pernah diisi
+                String[] header = {             //masukkan header
                     "Population Size",
                     "Mutation Rate",
                     "Crossover Rate",
@@ -136,7 +137,7 @@ public class Main {
                     "Max fitness",
                     "Test Case"
                 };
-                writer.writeNext(header);
+                writer.writeNext(header);   //masukkan header
             }
 
             //masukkan data eksperimen pada csv
@@ -151,11 +152,10 @@ public class Main {
                 Maxfitness,
                 TestCase
             };
-            writer.writeNext(data1);
+            writer.writeNext(data1);    //tulis hasil eksperimen pada file csv
 
             writer.close(); //close csvwriter
         } catch (IOException e) {
-
         }
     }
 
